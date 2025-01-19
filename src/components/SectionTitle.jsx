6eaf720fac27cce7ce00af7261218c0f1fd76798
kolade082 <koladeoluwadara@gmail.com>
2025-01-19 17:39:09 +0100

    section title for navigation pages created

---
+++ src/components/SectionTitle.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SectionTitle({ title, subtitle }) {
+  return (
+    <div className="section-title">
+      <h2>{title}</h2>
+      <p>{subtitle}</p>
+    </div>
+  );
+}
+
+export default SectionTitle;
