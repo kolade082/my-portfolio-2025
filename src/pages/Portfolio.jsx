cb87cf5c4cd7abf40d11b115cc1a2c4d458798a8
kolade082 <koladeoluwadara@gmail.com>
2025-01-22 12:44:10 +0100

    portfolio image displaying test work

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
-import './portfolio.css';
-import SectionTitle from '../components/SectionTitle';
+import React, { useState, useEffect } from "react";
+import "./portfolio.css";
+import SectionTitle from "../components/SectionTitle";
+import PortfolioItem from "../components/PortfolioItem";
 
 function Portfolio({ reference }) {
+  const [portfolio, setPortfolio] = useState([]);
+  const [data, setData] = useState([]);
+
+  const fetchData = () => {
+    fetch("http://localhost:3000/api/portfolioData.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setPortfolio(data);
+      })
+      .catch((e) => console.error("Error fetching portfolio data:", e.message));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    setData(portfolio);
+  }, [portfolio]);
+
   return (
     <section id="portfolio" className="portfolio" ref={reference}>
       <div className="container">
-        <SectionTitle title="Portfolio" subtitle="Under Construction 🚧" />
-        <div className="under-construction">
-          <i className="bx bx-wrench"></i>
-          <h2>Coming Soon!</h2>
+        <SectionTitle title="Portfolio" subtitle="Explore My Work" />
+
+        <div className="row portfolio-container">
+          {data && data.length > 0 ? (
+            data.map((item) => <PortfolioItem key={item._id} item={item} />)
+          ) : (
+            <p className="text-center">No portfolio items to display.</p>
+          )}
         </div>
       </div>
     </section>
